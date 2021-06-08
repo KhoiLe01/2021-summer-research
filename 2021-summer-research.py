0918ddfe83db9f1423b97fc240fb801ed92ca26c
Khoi Le <le_k2@denison.edu>
2021-06-08 13:44:10 -0400

    Fix for plotting

--- 2021-summer-research.py
+++ 2021-summer-research.py
@@ -3,7 +3,7 @@ import math
 
 def SET(machines, jobs, c):
     m = len(machines) - 1
-    for item in jobs:
+    for index, item in enumerate(jobs):
         kj = 0
         k = min(m, math.sqrt(item[0]/c))
         if k == m:
@@ -16,31 +16,38 @@ def SET(machines, jobs, c):
         sj = machines[kj-1][0]
         for j in range(kj):
             if machines[j][0] == 0:
-                machines[j].append(sj + (item[0]/(kj)+(kj-1)*c))
+                machines[j].append([sj + (item[0]/(kj)+(kj-1)*c), index+1])
             else:
-                machines[j].append(str(sj-machines[j][0]))
-                machines[j].append((item[0]/(kj)+(kj-1)*c))
+                machines[j].append([str(sj-machines[j][0]), 0])
+                machines[j].append([(item[0]/(kj)+(kj-1)*c), index+1])
             machines[j][0] = sj + (item[0]/(kj)+(kj-1)*c)
+    SET_makespan = makespan(machines)
+    for i in range(len(machines)):
+        machines[i] = machines[i][1:]
+    return SET_makespan
 
 def LPT(machines, jobs):
     jobs.sort(key=lambda jobs: jobs[0], reverse=True)
     min = machines[0][0]
     index = 0
-    for item in jobs:
+    for index2, item in enumerate(jobs):
         min = machines[index][0]
         for i in range (0, len(machines)):
             if machines[i][0] < min:
                 min = machines[i][0]
                 index = i
         insert_job(machines, item[0], index)
-        machines[index].append(item[0])
+        machines[index].append([item[0], index2+1])
+    LPT_makespan = makespan(machines)
+    for i in range(len(machines)):
+        machines[i] = machines[i][1:]
+    return LPT_makespan
 
 def makespan(machine_list):
-    max = 0
-    for i in range(1, len(machine_list)):
-        if machine_list[i][0] > max:
-            max = machine_list[i][0]
-    return max
+    return max([machine_list[i][0] for i in range(len(machine_list))])
+
+def makespan_machines(machine):
+    return sum([machine[i][0] for i in range(len(machine))])
 
 def insert_job(machine_list, job, machine_index):
     machine_list[machine_index][0] += job
@@ -53,11 +60,13 @@ def main(m, nj):
     for i in range(nj):
         jobs.append([random.randint(1,10)])
 
-    # LPT(machines, jobs)
-    # print(jobs)
-    # print(machines)
-    SET(machines, jobs, 1)
-    print(machines)
+    makespan = LPT(machines, jobs)
+    print(makespan)
+    for i in machines:
+        print(makespan_machines(i), i)
+    # SET(machines, jobs, 1)
+    # for i in machines:
+    #     print(i)
 
 main(5,8)
 
